18f3deff540a75ad2aa6dcd9bead92d6ca486492
bcm1995 <ytkcwl06@163.com>
2017-04-10 21:53:06 +0800

    搭建好基础功能框架

--- Account/app/src/main/java/com/bcm/account/AccountMainActivity.java
+++ Account/app/src/main/java/com/bcm/account/AccountMainActivity.java
@@ -1,10 +1,25 @@
 package com.bcm.account;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bcm.account.fragment.AddFragment;
+import com.bcm.account.fragment.DetailsFragment;
+import com.bcm.account.fragment.ReportFragment;
+import com.bcm.account.fragment.SelfFragment;
+import com.bcm.account.fragment.WalletFragment;
+import com.bcm.account.tools.NoScrollViewPager;
+import com.yinglan.alphatabs.AlphaTabView;
+import com.yinglan.alphatabs.AlphaTabsIndicator;
 
 import junit.framework.Test;
 
@@ -12,22 +27,72 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.QueryListener;
 
 public class AccountMainActivity extends AppCompatActivity {
     private TextView Tip;
+    private AlphaTabView tabView1, tabView2,tabView3,tabView4, tabView5;
+    protected int mWidth;
+    private DetailsFragment detailsFragment = new DetailsFragment();
+    private ReportFragment reportFragment = new ReportFragment();
+    private WalletFragment walletFragment = new WalletFragment();
+    private SelfFragment selfFragment = new SelfFragment();
+    private AddFragment addFragment = new AddFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_main);
         bindView();
         getDataFromBmob();
+        initData();
     }
     private void bindView(){
-        Tip = (TextView) findViewById(R.id.tip);
+//        Tip = (TextView) findViewById(R.id.tip);
     }
+
+    // 初始化数据
+
+    private void initData() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        mWidth = dm.widthPixels;
+        NoScrollViewPager viewPager = (NoScrollViewPager) findViewById(R.id.noscrollpager);
+        viewPager.setAdapter(new MainAdapter(getSupportFragmentManager()));
+        viewPager.setOffscreenPageLimit(5);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 1) {
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        AlphaTabsIndicator alphaIndicator = (AlphaTabsIndicator) findViewById(R.id.alphaIndicator);
+        alphaIndicator.setViewPager(viewPager);
+        tabView1 = (AlphaTabView) findViewById(R.id.AlphaView1);
+        tabView2 = (AlphaTabView) findViewById(R.id.AlphaView2);
+        tabView3 = (AlphaTabView) findViewById(R.id.AlphaView3);
+        tabView4 = (AlphaTabView) findViewById(R.id.AlphaView4);
+        tabView5 = (AlphaTabView) findViewById(R.id.AlphaView5);
+
+    }
+
     // 从Bmob 获取数据
     private void getDataFromBmob(){
         BmobQuery<Test> query = new BmobQuery("Test");
@@ -38,7 +103,7 @@ public class AccountMainActivity extends AppCompatActivity {
                     Log.i("array:",jsonArray.toString());
                     try {
                         JSONObject tips = jsonArray.getJSONObject(0);
-                        Tip.setText("成功连接Bmob："+tips.optString("test"));
+//                        Tip.setText("成功连接Bmob："+tips.optString("test"));
                     } catch (JSONException e1) {
                         e1.printStackTrace();
                     }
@@ -48,5 +113,40 @@ public class AccountMainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+
+        public MainAdapter(FragmentManager fm) {
+            super(fm);
+            fragments.add(detailsFragment);
+            fragments.add(walletFragment);
+            fragments.add(addFragment);
+            fragments.add(reportFragment);
+            fragments.add(selfFragment);
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ Account/app/src/main/java/com/bcm/account/fragment/DetailsFragment.java
@@ -0,0 +1,23 @@
+package com.bcm.account.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bcm.account.R;
+
+/**
+ * Created by Bean on 2017/4/10.
+ */
+
+public class DetailsFragment extends Fragment {
+    private View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_details_fragment, null);
+        return view;
+    }
+
+}

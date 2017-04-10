de213c2089d7c126186d658b257de18f3aeaf5d8
bcm1995 <ytkcwl06@163.com>
2017-04-10 14:46:46 +0800

    hello world

--- Account/app/src/main/java/com/bcm/account/AccountMainActivity.java
+++ Account/app/src/main/java/com/bcm/account/AccountMainActivity.java
@@ -2,6 +2,7 @@ package com.bcm.account;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AccountMainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class AccountMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_main);
+        Toast.makeText(getApplicationContext(),"!",Toast.LENGTH_SHORT).show();
     }
 }

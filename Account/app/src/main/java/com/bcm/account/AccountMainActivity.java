f94b4c58b94bd3f8bfe34882f11694428b662061
bcm1995 <ytkcwl06@163.com>
2017-04-10 14:37:41 +0800

    提交Account项目

---
+++ Account/app/src/main/java/com/bcm/account/AccountMainActivity.java
@@ -0,0 +1,13 @@
+package com.bcm.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_main);
+    }
+}

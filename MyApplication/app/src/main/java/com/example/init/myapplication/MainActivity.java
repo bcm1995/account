fe53c789f374293788d37a54ae14379cdc95a4d2
InitZyh <602683298@qq.com>
2017-04-10 13:56:50 +0800

    add hellowodrld

--- MyApplication/app/src/main/java/com/example/init/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/init/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.init.myapplication;
 
+import android.support.v4.view.accessibility.AccessibilityManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"hello world",Toast.LENGTH_LONG).show();
     }
 }
